29a9e27aa1c555766bea339f9c173b0770ee1282
PabloXberg <pabloxberg@gmail.com>
2023-03-30 13:53:55 +0200

    Figting with Slides and starting to rhink in quiz

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -8,14 +8,13 @@ function Show() {
   const location = useLocation();
   const { result: characters, error, loading } = useFetch('https://api.sampleapis.com/futurama/info', 'sc');
   const {user} = useContext(AuthContext);
-  console.log('characters :>> ', characters[0]);
+  // console.log('characters :>> ', characters[0]);
 
   return (
     <div>
       { location.pathname.includes("Characters") || location.pathname.includes("Episodes") ? <Outlet /> : 
       <>
-        <h1>The Show</h1>
-          <CarouselSlide Info={ characters && characters[0] } />
+          <CarouselSlide Info={ characters ? characters[0] : null} />
       </>
       }
     </div>
